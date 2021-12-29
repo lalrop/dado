39dc7ed4635a646647604665b2ffbec3c6b0c96a
lalrop <lalrop@hotmail.com>
2021-12-29 14:21:26 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let x = 0;
+
+function lanzarDado () {
+    $("#result").html("")
+    let valCaras = parseInt($("#numCaras")[0].value);
+    let numDados = parseInt($("#numDados")[0].value);
+   
+    while (x < numDados) {
+        x += 1;
+        let valRandom = Math.round(Math.random(0,1)*valCaras); 
+        var html = 
+        `<div class="d-flex bg-secondary mx-4 px-4 shadow p-3 mb-5 rounded "><h3 id="vRandom${x}">${valRandom}</h3></div>`;        
+        $("#result").append(html);       
+    }
+    x = 0
+
+ 
+}
+
+// Animacion! falta ver como detenerlas todas...
+// for (let i = 0; i < numDados+1; i++) {
+//     var interV;    
+//     interV = setInterval(() => {
+//         $(`#vRandom${i}`).text(Math.round(Math.random(0,1)*valCaras))
+//      },200);
+//      setTimeout(() => {
+//         clearInterval(interV) 
+//      },1000);
+// };
