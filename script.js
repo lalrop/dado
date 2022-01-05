2715a2468cc3cb5f2de4f55e0ed12a99ecd2bfa5
lalrop <lalrop@hotmail.com>
2022-01-05 11:21:55 -0300

    solucionando problema del 10 y cero. Ahora no muestra 0

--- script.js
+++ script.js
@@ -7,23 +7,15 @@ function lanzarDado () {
    
     while (x < numDados) {
         x += 1;
-        let valRandom = Math.round(Math.random(0,valCaras)*(0,9)); 
+        let valRandom = Math.round(Math.random(0,valCaras)*10); 
+        if (valRandom == 0) {
+            valRandom = Math.round(Math.random(0,valCaras)*10);
+        };
         var html = 
         `<div class="d-flex bg-secondary mx-4 px-4 shadow p-3 mb-5 rounded "><h3 id="vRandom${x}">${valRandom}</h3></div>`;        
-        $("#result").append(html);       
+        $("#result").append(html);
     }
     x = 0
 
  
 }
-
-// Animacion! falta ver como detenerlas todas...
-// for (let i = 0; i < numDados+1; i++) {
-//     var interV;    
-//     interV = setInterval(() => {
-//         $(`#vRandom${i}`).text(Math.round(Math.random(0,1)*valCaras))
-//      },200);
-//      setTimeout(() => {
-//         clearInterval(interV) 
-//      },1000);
-// };
